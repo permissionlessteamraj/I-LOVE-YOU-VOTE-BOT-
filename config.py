876f1ec8861df8e8ef5684513c51c18c6676adaa
permissionlessteamraj <permissionlessteam@gmail.com>
2025-08-30 13:40:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,4 +8,3 @@ WELCOME_IMAGE_URL = os.getenv("WELCOME_IMAGE_URL")
 
 if not all([TOKEN, ADMIN_ID, CHANNEL_ID, BOT_USERNAME, WELCOME_IMAGE_URL]):
     raise EnvironmentError("One or more environment variables are missing. Please set TOKEN, ADMIN_ID, CHANNEL_ID, BOT_USERNAME, and WELCOME_IMAGE_URL.")
-
